b7652a34ca92219fa2e92eac7e768455527a34b1
Burak Özdemir <75833960+Burakzdd@users.noreply.github.com>
2021-02-26 11:24:41 +0300

    Basic Linear Regresyon

---
+++ basitlinear.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 21:20:58 2021
+
+@author: burakzdd
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+dataset = pd.read_csv('/home/burakzdd/Desktop/makine öğrenmesi/basitlinear.csv')
+
+dataset.shape
+(25, 2)
+dataset.head()
+dataset.describe()
+dataset.plot(x='KisiSayisi', y='YuzdeBasari', style='.',color='green',ms= '20')
+plt.title('Kürek Çekme Yarışı Kişi ve Başarı')
+plt.xlabel('Kişi Sayisi')
+plt.ylabel('Başarı Yüzdesi')
+plt.show()
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+print(regressor.intercept_)
+print(regressor.coef_)
+y_pred = regressor.predict(X_test)
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+df
+from sklearn import metrics
+print('Ortalama Mutlak Hata:', metrics.mean_absolute_error(y_test, y_pred))
+print('Kare Ortalama Hata:', metrics.mean_squared_error(y_test, y_pred))
+print('Karekök ortalama hata:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
